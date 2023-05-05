57d991bf679566d1734713a9d88f92d9edcb35d1
Aibar-S <aibar.kamashev@gmail.com>
2023-05-05 20:41:06 +0400

    update 4

--- app_2.py
+++ app_2.py
@@ -8,7 +8,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
