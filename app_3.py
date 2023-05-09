45f88f33f88524e961c3df2aba1cf62e87d3a530
Aibar-S <aibar.kamashev@gmail.com>
2023-05-09 20:51:50 +0400

    update 26

--- app_3.py
+++ app_3.py
@@ -6,8 +6,6 @@ from sklearn.ensemble import RandomForestClassifier
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
