04e4600f7c7c8507da184171288a99313ed95ea5
Aibar-S <aibar.kamashev@gmail.com>
2023-05-20 15:47:57 +0400

    41

--- app_interface.py
+++ app_interface.py
@@ -22,55 +22,31 @@ def main():
         picture_path = 'drilling_rig.JPG'  # Replace with the path to your picture
         picture = Image.open(picture_path)
 
-        # Create an empty slot in the sidebar
-        sidebar_slot = st.sidebar.empty()
+        # Define the CSS style to control the size of the picture and the sidebar
+        picture_style = f"width: 100%; max-width: 800px; height: auto;"
+        sidebar_style = f"width: 200px;"
 
-        # Calculate the maximum width and height to fit inside the slot
-        max_width = sidebar_slot.width - 50  # Subtracting some padding
-        max_height = sidebar_slot.height - st.title_height() - 100  # Subtracting title height and some padding
-
-        # Rescale the picture to fit inside the slot
-        picture.thumbnail((max_width, max_height), Image.ANTIALIAS)
-
-        # Create a drawing object
-        draw = ImageDraw.Draw(picture)
-
-        # Define the arrow start and end points
-        arrow_start_points = [(100, 200), (300, 400), (500, 600)]  # Replace with the coordinates of your arrows
-        arrow_end_points = [(150, 250), (350, 450), (550, 650)]  # Replace with the coordinates of your arrows
-
-        # Define the font for the part names
-        font = ImageFont.truetype("ArianaVioleta-dz2K.ttf", size=16)  # Replace with the path to your font file and desired font size
-
-        # Draw arrows and part names on the picture
-        for i, (start_point, end_point) in enumerate(zip(arrow_start_points, arrow_end_points)):
-            part_name = list(parts.keys())[i]
-
-            # Draw arrow
-            draw.line(start_point + end_point, fill="red", width=3)
-            draw.polygon([end_point, (end_point[0] - 10, end_point[1] + 5), (end_point[0] + 10, end_point[1] + 5)], fill="red")
-
-            # Draw part name
-            text_width, text_height = draw.textsize(part_name, font=font)
-            text_position = (start_point[0] + (end_point[0] - start_point[0]) // 2 - text_width // 2, start_point[1] - text_height - 10)
-            draw.text(text_position, part_name, font=font, fill="black")
-
-        # Display the picture with arrows and part names
-        st.image(picture, use_column_width=True)
-
-        st.write("Click on the names to view the description of each part.")
+        # Apply the CSS style to the picture and the sidebar
+        st.markdown(f'<img src="data:image/jpeg;base64,{image_to_base64(picture)}" style="{picture_style}">', unsafe_allow_html=True)
+        st.sidebar.markdown(f'<div style="{sidebar_style}">', unsafe_allow_html=True)
 
         # Display the descriptions
-        for i, (start_point, end_point) in enumerate(zip(arrow_start_points, arrow_end_points)):
-            part_name = list(parts.keys())[i]
+        for part_name, description in parts.items():
+            if st.sidebar.button(part_name):
+                st.write(description)
 
-            # Get the description of the part when its name is clicked
-            if st.button(part_name):
-                st.write(parts[part_name])
+        st.sidebar.markdown("</div>", unsafe_allow_html=True)
 
     else:
         st.title("About")
         st.write("This app was created by [Your Name] as a Streamlit exercise.")
+
+def image_to_base64(image):
+    from io import BytesIO
+    import base64
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    return base64.b64encode(buffered.getvalue()).decode()
 
 if __name__ == "__main__":
     main()
