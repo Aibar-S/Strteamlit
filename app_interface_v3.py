9ff1ed5448708eba707152df3dc53670b6aff33c
Aibar-S <aibar.kamashev@gmail.com>
2023-05-20 17:59:32 +0400

    57

--- app_interface_v3.py
+++ app_interface_v3.py
@@ -95,17 +95,17 @@ def main():
         picture_url = 'Pictures/Drilling_pic_3.JPG'  # Replace with the URL of your picture
         st.image(picture_url, use_column_width=False)
         
-        st.write("Click on the buttons to view the description of each part.")
+        st.write("Click on the buttons to view the description of each parameter.")
         
         # Define the parts of the picture and their descriptions
         parts = {
-            "1-Measured Depth": "Measured depth along the well trajectory",
+            "**:blue[1-Measured Depth**": "Measured depth along the well trajectory",
             "2-Hook Load": "The total force pulling down on the hook. This total force includes the weight of the drillstring in air and any other equipment, reduced by any force that tends to reduce that weight (friction along the wellbore wall, buoyant forces on the drillstring caused by its immersion in drilling fluid.",
             "3-Rotary RPM": "Revolutions per minute. A rotary table is a mechanical device on a drilling rig that provides clockwise rotational force to the drill string. Rotary speed is the number of times the rotary table makes one full revolution in one minute (rpm).",
             "4-Rotary Torque": "The force required to rotate the entire drill string and the drilling bit at the bottom of the hole to overcome the rotational friction against the wellbore, the viscous force between the drill string and drilling fluid as well as the drilling bit torque.",
             "5-Weight on Bit": "The amount of downward force exerted on the drilling bit by drill pipes to break a rock.",
             "6-Differential Pressure": "The difference in pressure between the hydrostatic head of the drilling fluid in the fluid column, and the pressure exerted from the formation at any given depth in the hole. May be positive, zero, or negative with respect to the hydrostatic head.",
-            "7-Gamma at Bit": "The natural radioactivity in a formation used to classify lithologies and correlate zones.",
+            "7-Gamma at Bit": "The natural radioactivity in a formation used to classify lithologies and correlate zones. It shows the potential lithologies which can contain oil and gas",
         }
         
         # Display the buttons and descriptions
