b8c305fd66da54cf3b7c48d71199c05347de50e4
Aibar-S <aibar.kamashev@gmail.com>
2023-05-20 16:09:29 +0400

    45

--- app_interface_v2.py
+++ app_interface_v2.py
@@ -2,35 +2,38 @@ import streamlit as st
 
 def main():
     st.set_page_config(page_title="Picture Description App", layout="wide")
-    
+
     st.sidebar.title("Tabs")
     tabs = ["Picture", "About"]
     selected_tab = st.sidebar.selectbox("Select a tab", tabs)
-    
+
     if selected_tab == "Picture":
         st.title("Picture")
         st.write("Here is the picture:")
-        
+
         picture_url = 'drilling_rig.JPG'  # Replace with the URL of your picture
-        st.image(picture_url, width="100%")
         
+        # Display the picture with full-width container
+        with st.beta_container():
+            st.image(picture_url, use_column_width=True)
+
         st.write("Click on the buttons to view the description of each part.")
-        
+
         # Define the parts of the picture and their descriptions
         parts = {
             "Part A": "Description of Part A.",
             "Part B": "Description of Part B.",
             "Part C": "Description of Part C.",
         }
-        
+
         # Display the buttons and descriptions
         for part_name, description in parts.items():
             if st.button(part_name):
                 st.write(description)
-    
+
     else:
         st.title("About")
         st.write("This app was created by [Your Name] as a Streamlit exercise.")
-    
+
 if __name__ == "__main__":
     main()
