1411f22d30e2a43693a86133277b3f6f3c590eb1
Aibar-S <aibar.kamashev@gmail.com>
2023-05-20 15:58:38 +0400

    43

--- app_interface_v2.py
+++ app_interface_v2.py
@@ -14,7 +14,7 @@ def main():
         picture_url = 'drilling_rig.JPG'  # Replace with the URL of your picture
         st.image(picture_url, use_column_width=True)
         
-        st.write("Click on the names to view the description of each part.")
+        st.write("Click on the buttons to view the description of each part.")
         
         # Define the parts of the picture and their descriptions
         parts = {
@@ -23,17 +23,10 @@ def main():
             "Part C": "Description of Part C.",
         }
         
-        # Display the part names and descriptions
+        # Display the buttons and descriptions
         for part_name, description in parts.items():
-            col1, col2 = st.columns([0.4, 0.6])
-            
-            with col1:
-                st.write(part_name)
-            
-            with col2:
-                # Get the description of the part when its name is clicked
-                if st.button(part_name):
-                    st.write(description)
+            if st.button(part_name):
+                st.write(description)
     
     else:
         st.title("About")
